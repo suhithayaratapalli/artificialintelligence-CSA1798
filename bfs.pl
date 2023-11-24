6f58b4217c0c38b51eb162dfe47667e8af9484ad
suhithayaratapalli <122381912+suhithayaratapalli@users.noreply.github.com>
2023-11-24 14:20:47 +0530

    Create bfs.pl

---
+++ bfs.pl
@@ -0,0 +1,15 @@
+% Facts representing a directed graph
+edge(a, b).
+edge(a, c).
+edge(b, d).
+edge(b, e).
+edge(c, f).
+edge(c, g).
+
+% Breadth-first search
+bfs(Start, Goal) :- bfs_queue([[Start]], Goal).
+
+bfs_queue([[Node|Path]|_], Node) :- reverse([Node|Path], Solution), write(Solution), nl.
+bfs_queue([Path|Paths], Goal) :- extend(Path, NewPaths), append(Paths, NewPaths, Queue), bfs_queue(Queue, Goal).
+
+extend([Node|Path], NewPaths) :- findall([NewNode, Node|Path], (edge(Node, NewNode), \+ member(NewNode, [Node|Path])), NewPaths).
